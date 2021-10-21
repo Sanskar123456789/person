e590bb5b8172b518fe77ba2ad63e0ac1b04699aa
Sanskar123456789 <sanskar.agarwal91@gmail.com>
2021-10-21 19:42:14 +0530

    initial commit

---
+++ apps/person/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'person-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'person';
+}
